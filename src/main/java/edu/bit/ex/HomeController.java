2200a895cffbdf8869352af39cad1160a39b27a9
dlskfo306 <dlskfo306@gmail.com>
2021-06-14 13:19:19 +0900

    csrf commit

--- src/main/java/edu/bit/ex/HomeController.java
+++ src/main/java/edu/bit/ex/HomeController.java
@@ -49,7 +49,7 @@ public class HomeController {
     @GetMapping("/login/loginForm")
     public String loginForm() {
         log.info("Welcome Login Form");
-        return "login/loginForm2";
+        return "login/loginForm";
     }
     
     
