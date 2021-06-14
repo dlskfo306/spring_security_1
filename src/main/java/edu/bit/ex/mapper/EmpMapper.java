f06da79577eb972a6b725d691460f1400e6440f1
dlskfo306 <dlskfo306@gmail.com>
2021-06-14 11:39:08 +0900

    step9 수정

--- src/main/java/edu/bit/ex/mapper/EmpMapper.java
+++ src/main/java/edu/bit/ex/mapper/EmpMapper.java
@@ -2,6 +2,7 @@ package edu.bit.ex.mapper;
 
 import org.apache.ibatis.annotations.Insert;  
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import edu.bit.ex.vo.EmpVO;
 import edu.bit.ex.vo.MemberVO;
@@ -9,6 +10,8 @@ import edu.bit.ex.vo.UserVO;
 
 @Mapper
 public interface EmpMapper {
+    
+    @Select("select * from emp where ename = #{ename}")
     public EmpVO getEmp(String ename);
     //ename = ID
 }
